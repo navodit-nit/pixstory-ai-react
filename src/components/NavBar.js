f7392678c109e423bfaf6746a54631f353da937d
sunilkush <sunilkushwaha066@gmail.com>
2023-10-03 12:10:54 +0530

    hide navbar in toggle

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -20,14 +20,13 @@ const NavBar = (props) => {
                   className="dark-logo"
                 />
               </a>
-              <button className="btn float-end">
+              {/* <button className="btn float-end">
               <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path d="M1 12H23" stroke="" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                   <path d="M1 4H23" stroke="" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                   <path d="M1 20H23" stroke="" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                </svg>             
-                
-              </button>
+                </svg>     
+              </button> */}
               <ReactSwitch onChange={props.toggleTheme} checked={props.theme === "dark"} />
              </div>
             </div>
